3d0a4e30b51114cc12e28982cc3b6606816fbc37
Yomaira Escano <yjescano@gmail.com>
2019-02-04 20:57:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 // Code your solution in this file
-function findMatching(drivers, driver) {
-  return drivers.filter(function (name) {
-    return driver.toUpperCase() === name.toUpperCase();
-  });
-}
+// function findMatching(drivers, driver) {
+//   return drivers.filter(function (name) {
+//     return driver.toUpperCase() === name.toUpperCase();
+//   });
+// }
+// 
+// function fuzzyMatch(drivers, driver) {
+//   return drivers.filter(function (name) {
+//     return name.slice(0, driver.length) === driver;
+//   });
+// }
+// 
+// function matchName(drivers, name) {
+//   return drivers.filter(function (driver) {
+//     return driver.name.toUpperCase() === name.toUpperCase();
+//   })
+// }
 
-function fuzzyMatch(drivers, driver) {
-  return drivers.filter(function (name) {
-    return name.slice(0, driver.length) === driver;
-  });
-}
-
-function matchName(drivers, name) {
+unction findMatching(drivers, string) {
   return drivers.filter(function (driver) {
-    return driver.name.toUpperCase() === name.toUpperCase();
+    return string.toUpperCase() === newDriver.toUpperCase();
+  })
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (driver) {
+    return string[0] === newDriver[0];
+  })
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function (driver) {
+    return string === newDriver['name'];
   })
 }
