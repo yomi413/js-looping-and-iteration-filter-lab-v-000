e2197ac9e95ae3ff4ce7b55ca8a7f6141abc92f4
Yomaira Escano <yjescano@gmail.com>
2019-02-04 20:57:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 //     return driver.toUpperCase() === name.toUpperCase();
 //   });
 // }
-// 
+//
 // function fuzzyMatch(drivers, driver) {
 //   return drivers.filter(function (name) {
 //     return name.slice(0, driver.length) === driver;
 //   });
 // }
-// 
+//
 // function matchName(drivers, name) {
 //   return drivers.filter(function (driver) {
 //     return driver.name.toUpperCase() === name.toUpperCase();
